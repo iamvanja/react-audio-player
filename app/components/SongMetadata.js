dac64797f70bcb75b3a806239caa25fb1d6b3a85
iamvanja <vanja@gavric.org>
2017-04-17 00:47:20 -0700

    Made player responsive, text truncating for SongMetadata, Tweaked control button size

--- app/components/SongMetadata.js
+++ app/components/SongMetadata.js
@@ -4,7 +4,9 @@ import PropTypes from 'prop-types';
 const SongMetaData = ({ title, albumName, artist }) => {
     return (
         <div className="song-metadata">
-            <h1 className="info-item primary-content title">{title}</h1>
+            <div className="primary-container">
+                <h1 className="info-item primary-content title">{title}</h1>
+            </div>
             <h2 className="info-item secondary-content album">{albumName}</h2>
             <h2 className="info-item secondary-content artist">{artist}</h2>
         </div>
